a9157774a035f8d5500c03e71c416ae34e0fc5cb
nasywasyifa <ebenhaiser@gmail.com>
2024-11-22 14:35:38 +0700

    edit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.layout_admin.app')
+@section('conteeennnn')
+tessss
+@endsection
---
+++ resources/views/layouts/layout_admin/app.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin</title>
+
+    <!-- Custom fonts for this template-->
+    @include('layouts.layout_admin.inc.style')
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        @include('layouts.layout_admin.inc.sidebar')
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                @include('layouts.layout_admin.inc.topbar')
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                    @yield('conteeennnn')
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            @include('layouts.layout_admin.inc.footer')b
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    @include('layouts.layout_admin.inc.scroll-to-top-button')
+
+    <!-- Logout Modal-->
+    @include('layouts.layout_admin.inc.logout-modal')
+
+    <!-- Bootstrap core JavaScript-->
+    @include('layouts.layout_admin.inc.script')
+
+</body>
+
+</html>
